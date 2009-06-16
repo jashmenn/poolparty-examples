856ca29869ca6d0a6be82517da420116e1c8b85b
Nate Murray <nate@natemurray.com>
2009-06-16 09:08:19 -0700

    tweaks, cleanup to hadoop config

--- hadoop/clouds.rb
+++ hadoop/clouds.rb
@@ -2,6 +2,30 @@ require 'plugins/hadoop/hadoop'
 require 'poolparty-extensions'
 
 pool(:hadoop_cluster) do
+
+  cloud(:hadoop_slave) do
+    instances 1
+
+    using :ec2
+    keypair "nmurray-hadoop-slave"
+
+    has_package "tree"
+    has_package "vim-nox"
+    has_package" screen"
+    has_package" irb"
+    has_bash_alias :name => "inspect-poolparty-recipes", :value => "vi /var/poolparty/dr_configure/chef/cookbooks/poolparty/recipes/default.rb"
+    has_bash_alias :name => "cd-cookbooks", :value => "pushd /var/poolparty/dr_configure/chef/cookbooks/poolparty"
+
+    hadoop do
+      # run_example_job
+    end
+
+    # clouds[:hadoop_master].nodes(:status => 'running').each_with_index do |n, i|
+    #   has_host(:name => "master#{i}", :ip => n.public_ip) 
+    # end
+
+  end # cloud :hadoop_slave
+
   cloud(:hadoop_master) do
     instances 1
 
@@ -13,48 +37,26 @@ pool(:hadoop_cluster) do
 
     using :ec2
     keypair "nmurray-hadoop-master"
-    # keypair "hadoop-master"
- 
 
     has_package "tree"
     has_package "vim-nox"
     has_package" screen"
     has_package" irb"
     has_bash_alias :name => "inspect-poolparty-recipes", :value => "vi /var/poolparty/dr_configure/chef/cookbooks/poolparty/recipes/default.rb"
+    has_bash_alias :name => "cd-cookbooks", :value => "pushd /var/poolparty/dr_configure/chef/cookbooks/poolparty"
 
     hadoop do
-      # run_example_job
+      configure_master
     end
 
-    has_host(:name => "master", :ip => "127.0.0.1")
-    # clouds[:hadoop_slave].nodes(:status => 'running').each_with_index do |n, i|
-    #   has_host(:name => "slave#{i}", :ip => n.public_ip) 
-    # end
+    # todo, this should be a list of all the masters
+    has_host(:name => "master0", :ip => "127.0.0.1")
+    clouds[:hadoop_slave].nodes(:status => 'running').each_with_index do |n, i|
+      has_host(:name => "slave#{i}", :ip => n.public_ip) 
+    end
 
   end # cloud :hadoop_master
 
-  cloud(:hadoop_slave) do
-    instances 1
-
-    using :ec2
-    keypair "nmurray-hadoop-slave"
-    # keypair "hadoop-slave"
-
-    has_package "tree"
-    has_package "vim-nox"
-    has_package" screen"
-    has_package" irb"
-    has_bash_alias :name => "inspect-poolparty-recipes", :value => "vi /var/poolparty/dr_configure/chef/cookbooks/poolparty/recipes/default.rb"
-
-    hadoop do
-      # run_example_job
-    end
-
-    clouds[:hadoop_master].nodes(:status => 'running').each_with_index do |n, i|
-      has_host(:name => "master#{i}", :ip => n.public_ip) 
-    end
-
-  end # cloud :hadoop_slave
 
 end # pool
 
