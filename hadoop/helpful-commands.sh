3a8b96c977f7c359c5c05028f512f4da2e53a1ec
Nate Murray <nate@natemurray.com>
2009-06-17 12:51:11 -0700

    added some helpful commands

---
+++ hadoop/helpful-commands.sh
@@ -0,0 +1,9 @@
+sudo -u hadoop -H /usr/local/hadoop/bin/hadoop dfs -rmr gutenberg-output
+sudo -u hadoop -H /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/hadoop-0.20.0-examples.jar wordcount gutenberg gutenberg-output
+sudo -u hadoop -H ./bin/hadoop dfs -copyFromLocal /tmp/gutenberg gutenberg
+sudo -u hadoop -H ./bin/stop-all.sh 
+sudo -u hadoop -H ./bin/start-dfs.sh
+sudo -u hadoop -H ./bin/start-mapred.sh
+ec2-run-instances -k nmurray-hadoop-slave ami-bf5eb9d6
+http://ip:50075/browseDirectory.jsp?namenodeInfoPort=50070&dir=%2F
+grep -R port *|  grep -vi import | grep -vi report | grep -vi support
