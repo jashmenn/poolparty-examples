1e65b6b0bde0c83ad15f2d2cd6ec9b75bb3deaac
Nate Murray <nate@natemurray.com>
2009-06-18 10:34:15 -0700

    added stub for ganglia

---
+++ hadoop/plugins/ganglia/ganglia.rb
@@ -0,0 +1,15 @@
+=begin rdoc
+=end
+
+module PoolParty
+  module Plugin
+    class Ganglia < Plugin
+      def before_load(o={}, &block)
+        do_once do
+        end
+      end
+    end
+  end
+end
+
+
