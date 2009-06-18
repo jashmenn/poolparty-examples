012b088b9c05559e236a8acb4933c94aed67ae85
Nate Murray <nate@natemurray.com>
2009-06-18 11:12:18 -0700

    added a few more packages

--- hadoop/plugins/ganglia/ganglia.rb
+++ hadoop/plugins/ganglia/ganglia.rb
@@ -18,16 +18,28 @@ module PoolParty
       def before_load(o={}, &block)
         do_once do
           install_dependencies
+          configs
         end
       end
 
       def install_dependencies
         has_package :name => "rrdtool"
+        has_package :name => "libganglia1"
+        has_package :name => "ganglia-monitor"
       end
 
       def install_webserver_configs
         # hmm
       end
+
+      def configs 
+        has_variable "ganglia_cloud_name", :value => name
+        has_file(:name => "/etc/gmond.conf") do
+          mode 0644
+          template :plugins/:ganglia/:templates/"gmond.conf.erb"
+        end
+      end
+
     end
   end
 end
