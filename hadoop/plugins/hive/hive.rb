fd85c5f4b6d5407d226b1aa1c787547d403ae7c0
Nate Murray <nate@natemurray.com>
2009-06-18 09:56:59 -0700

    added plugin to support hive

---
+++ hadoop/plugins/hive/hive.rb
@@ -0,0 +1,102 @@
+=begin rdoc
+=end
+
+module PoolParty
+  module Plugin
+    class Hive < Plugin
+      def before_load(o={}, &block)
+        do_once do
+          install_from_bin
+          set_environment_variables
+          create_hdfs_directories
+        end
+      end
+
+      def install_from_bin
+        has_exec "wget #{hive_dist} -O /usr/local/src/hive-0.3.0-hadoop-0.19.0-dev.tar.gz",
+          :not_if => "test -e /usr/local/src/hive-0.3.0-hadoop-0.19.0-dev.tar.gz"
+        has_exec "cd /usr/local/src && tar -xvvf /usr/local/src/hive-0.3.0-hadoop-0.19.0-dev.tar.gz", 
+          :not_if => "test -f #{hive_home}"
+        has_exec "mv /usr/local/src/hive-0.3.0-hadoop-0.19.0-bin #{hive_home}", 
+          :not_if => "test -f #{hive_home}"
+      end
+
+      # doesn't really work
+      def install_from_src
+        install_dependent_packages
+        download_and_build_src
+      end
+
+      def install_dependent_packages
+        has_package :name => "subversion"
+        has_package :name => "ant"
+      end
+
+      def download_and_build_src
+        has_exec "svn co #{hive_repo} #{src_dir}",
+          :not_if => "test -e #{src_dir}/build.xml"
+        has_exec "cd #{src_dir} && ant -Dhadoop.version=\\\"#{hadoop_version}\\\" package",
+          :not_if => "test -e #{src_dir}/build/dist/README.txt"
+      end
+
+      # todo, pull from parent
+      def set_environment_variables
+        has_file :name => "/root/.hadoop-etc-env.sh", :content => <<-EOF
+export HADOOP_HOME=#{hadoop_home}
+export HADOOP=$HADOOP_HOME/bin/hadoop
+export HIVE_HOME=#{hive_home}
+export PATH=$HADOOP_HOME/bin:$HIVE_HOME/bin:$PATH
+        EOF
+        has_line_in_file :file => "/root/.profile", :line => "source /root/.hadoop-etc-env.sh"
+      end
+
+      def create_hdfs_directories
+        has_exec "#{hadoop_home}/bin/hadoop fs -mkdir /tmp", 
+          :not_if => "#{hadoop_home}/bin/hadoop fs -ls /tmp"
+ 
+        has_exec "#{hadoop_home}/bin/hadoop fs -mkdir /user/hive/warehouse", 
+          :not_if => "#{hadoop_home}/bin/hadoop fs -ls /user/hive/warehouse"
+
+        has_exec "#{hadoop_home}/bin/hadoop fs -chmod g+w /tmp", 
+          :not_if => "#{hadoop_home}/bin/hadoop fs -ls /tmp" # todo, check perms
+ 
+        has_exec "#{hadoop_home}/bin/hadoop fs -chmod g+w /user/hive/warehouse", 
+          :not_if => "#{hadoop_home}/bin/hadoop fs -ls /user/hive/warehouse"
+      end
+
+      private
+
+      def hive_dist
+        "http://www.apache.org/dist/hadoop/hive/hive-0.3.0/hive-0.3.0-hadoop-0.19.0-dev.tar.gz"
+      end
+
+      def src_dir
+        "/usr/local/src/hive"
+      end
+
+      def hive_home
+        "/usr/local/hive"
+      end
+
+      def hive_repo
+        # "http://svn.apache.org/repos/asf/hadoop/hive/trunk"
+        "http://svn.apache.org/repos/asf/hadoop/hive/tags/release-0.3.0/"
+      end
+
+      ### should pull from parent
+      def hadoop_home
+        "/usr/local/hadoop"
+      end
+
+      # would be really awesome if this was a variable in the hadoop plugin and
+      # this called out to it via parent. todo
+      def hadoop_version
+        "0.20.0"
+      end
+
+
+
+    end
+  end
+end
+ 
