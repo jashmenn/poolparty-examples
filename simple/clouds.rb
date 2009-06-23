042055373ccbd256cec1b93a140e5653748963cb
Ari Lerner <arilerner@mac.com>
2009-06-23 15:47:25 -0700

    Updated simple clouds to test on metavirt

--- simple/clouds.rb
+++ simple/clouds.rb
@@ -4,6 +4,16 @@ pool :poolparty do
   
   cloud :app do
     instances 2..5
+    
+    using :metavirt do
+      server_config :host => "http://okra", :port => 3000
+      using :libvirt do
+        image_id "mvi_ef77fdf0"
+      end
+    end
+    
+    has_file "/etc/motd", :content => "From metavirt"
+    
   end
 
 end
