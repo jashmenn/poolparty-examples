67642c932ec3b8987f4a2645d34a202dee7b738f
Ari Lerner <arilerner@mac.com>
2009-07-31 20:38:55 -0700

    Confirmed the secure cloud passes

--- secure/clouds.rb
+++ secure/clouds.rb
@@ -38,10 +38,12 @@ pool(:cloud) do
   cloud(:secure) do
     instances 1
 
-    using :vmrun do
-      vmx_hash({
-        "/Users/nmurray/Documents/VMware/Ubuntu-jaunty.vmwarevm/Ubuntu-jaunty.vmx" => "192.168.133.128"
-      })
+    # replace this with the IP address of your vmware instance
+    keypair "id_rsa"
+    
+    using :vmware do
+      image_id "/Users/alerner/Documents/vm/Ubuntu32bitVM.vmwarevm/Ubuntu32bitVM.vmx"
+      public_ip "192.168.248.133"
     end
 
     # using :ec2 do
