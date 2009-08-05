b5a32d9fdf1ad4f5ff04ca6b5fde7a270a89978f
Ari Lerner <arilerner@mac.com>
2009-08-05 14:21:16 -0700

    Verified all work with 'lite' 1.3RC1 release

--- simple/clouds.rb
+++ simple/clouds.rb
@@ -5,11 +5,11 @@ pool :poolparty do
   cloud :app do
     instances 2..5
     
-    using :metavirt do
-      server_config :host => "http://okra", :port => 3000
-      using :libvirt do
-        image_id "mvi_ef77fdf0"
-      end
+    keypair "id_rsa"
+    
+    using :vmware do
+      image_id "/Users/alerner/Documents/vm/Ubuntu32bitVM.vmwarevm/Ubuntu32bitVM.vmx"
+      public_ip "192.168.248.133"
     end
     
     has_file "/etc/motd", :content => "From metavirt"
