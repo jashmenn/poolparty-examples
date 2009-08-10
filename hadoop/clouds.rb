e6043c96888aa76f06fa00381776b25ccb4c896e
Nate Murray <nate@natemurray.com>
2009-08-10 12:48:30 -0700

    tweaks for deving

--- hadoop/clouds.rb
+++ hadoop/clouds.rb
@@ -4,6 +4,16 @@ require 'rubygems'
 require "poolparty"
 require 'poolparty-extensions'
 
+%w{andand}.each do |gemname|
+  begin
+    require gemname
+  rescue LoadError
+    puts "This clouds.rb requires '#{gemname}': gem install #{gemname}"
+    exit
+  end
+end
+
+
 KEYPAIR_PREFIX = "cloud_hadoop"
 SECURITY_GROUP = "hadoop_pool"
 # KEYPAIR_PREFIX = "cloudteam_hadoop"
@@ -11,61 +21,61 @@ SECURITY_GROUP = "hadoop_pool"
 
 pool(:cloud) do
 
-  cloud(:hadoop_slave) do
-    instances 3
+  # cloud(:hadoop_slave) do
+  #   instances 3
 
-    keypair "#{KEYPAIR_PREFIX}_slave"
-    using :ec2 do
-      security_group [SECURITY_GROUP]
-    end
+  #   keypair "#{KEYPAIR_PREFIX}_slave"
+  #   using :ec2 do
+  #     security_group [SECURITY_GROUP]
+  #   end
 
-    has_convenience_helpers
-    has_gem_package("bjeanes-ghost")
-    has_gem_package("technicalpickles-jeweler")
-    has_development_gem('poolparty-extensions', :from => "#{File.dirname(__FILE__)}/../../poolparty-extensions")
+  #   has_convenience_helpers
+  #   has_gem_package("bjeanes-ghost")
+  #   has_gem_package("technicalpickles-jeweler")
+  #   has_development_gem('poolparty-extensions', :from => "#{File.dirname(__FILE__)}/../../poolparty-extensions")
 
-    apache do
-      enable_php5 # todo, write a phpinfo.php verifier
-    end
+  #   apache do
+  #     enable_php5 # todo, write a phpinfo.php verifier
+  #   end
 
-    hadoop do
-      configure_slave
-    end
+  #   hadoop do
+  #     configure_slave
+  #   end
 
-    ganglia do
-      slave
-      track :hadoop
-    end
+  #   ganglia do
+  #     slave
+  #     track :hadoop
+  #   end
 
-    has_package "nmap"
+  #   has_package "nmap"
 
-    tripwire do
-      root_dir "/usr/wpt" # CHANGE THIS to something obscure. The idea is you *dont* want tripwire to be in a standard location.
-      mailto   "admin@emaildomain"
-      smtp_settings "host", "username", "password"
-    end
+  #   tripwire do
+  #     root_dir "/usr/wpt" # CHANGE THIS to something obscure. The idea is you *dont* want tripwire to be in a standard location.
+  #     mailto   "admin@emaildomain"
+  #     smtp_settings "host", "username", "password"
+  #   end
 
-    shorewall do
-      rule "Web/ACCEPT net $FW"
-      rule "SSH/ACCEPT net $FW"
-      rule "ACCEPT net:10.0.0.0/8 $FW"    # allow local EC2 traffic OR
-    end
-    denyhosts
+  #   shorewall do
+  #     rule "Web/ACCEPT net $FW"
+  #     rule "SSH/ACCEPT net $FW"
+  #     rule "ACCEPT net:10.0.0.0/8 $FW"    # allow local EC2 traffic OR
+  #   end
+  #   denyhosts
 
-  end # cloud :hadoop_slave
+  # end # cloud :hadoop_slave
 
   cloud(:hadoop_master) do
     instances 1
 
-    # using :vmrun do
-    #   vmx_hash({
-    #     "/Users/nmurray/Documents/VMware/Ubuntu-jaunty.vmwarevm/Ubuntu-jaunty.vmx" => "192.168.133.128"
-    #   })
-    # end
-
-    using :ec2 do
-      security_group [SECURITY_GROUP]
+    using :vmrun do
+      vmx_hash({
+        "/Users/nmurray/Documents/VMware/Ubuntu-jaunty.vmwarevm/Ubuntu-jaunty.vmx" => "192.168.133.128"
+      })
     end
+
+    # using :ec2 do
+    #   security_group [SECURITY_GROUP]
+    # end
 
     keypair "#{KEYPAIR_PREFIX}_master"
 
@@ -92,8 +102,8 @@ pool(:cloud) do
       configure_master
       prep_example_job
       # run_example_job
-      create_client_user('hadoop_client') # NOTE! this creates a hadoop_client user! no password 
-                                          # login or authorized keys though, just thought you should know
+      # create_client_user('hadoop_client') # NOTE! this creates a hadoop_client user! no password 
+                                            # login or authorized keys though, just thought you should know
     end
 
     hive do
@@ -103,11 +113,10 @@ pool(:cloud) do
       has_gem_package "sequel", :jruby => true
     end
 
-    ganglia do
-      monitor "hadoop_slave", "hadoop_master" # what cloud names to monitor
-      master
-      # other things id like to monitor:
-    end
+    # ganglia do
+    #   monitor "hadoop_slave", "hadoop_master" # what cloud names to monitor
+    #   master
+    # end
 
     has_package "nmap"
     has_package "git-core"
@@ -131,28 +140,28 @@ pool(:cloud) do
     end
     denyhosts
 
-    mysql do
-      master
-      root_password "angrybobby123"
-      database do
-        create_database "metastore_db" 
-        grant_permissions_on_cloud_with_keypair_for_user "cloudteam_hadoop_master", "bobby", "abbyzabby17"
-        grant_permissions_on_host_for_user "localhost", "bobby", "abbyzabby17"
-      end
-    end
+    # mysql do
+    #   master
+    #   root_password "angrybobby123"
+    #   database do
+    #     create_database "metastore_db" 
+    #     grant_permissions_on_cloud_with_keypair_for_user "cloudteam_hadoop_master", "bobby", "abbyzabby17"
+    #     grant_permissions_on_host_for_user "localhost", "bobby", "abbyzabby17"
+    #   end
+    # end
 
   end # cloud :hadoop_master
 
   after_all_loaded do
     # get the master ips on the slaves
-    clouds[:hadoop_slave].run_in_context do
+    clouds[:hadoop_slave].andand.run_in_context do
       hadoop.perform_just_in_time_operations
       ganglia.perform_after_all_loaded_for_slave
     end
 
-    clouds[:hadoop_master].run_in_context do
+    clouds[:hadoop_master].andand.run_in_context do
       hadoop.perform_just_in_time_operations
-      ganglia.perform_after_all_loaded_for_master
+      # ganglia.perform_after_all_loaded_for_master
     end
   end
 
